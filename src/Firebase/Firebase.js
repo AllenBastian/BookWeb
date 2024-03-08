3fc6bca4dedc1f8e3846beff3b7bac4392d603e7
AllenBastian <reachallenbastian@gmail.com>
2024-03-08 12:00:07 +0530

    added firebase

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyALYWGBArvc3uSqThtiFj3C-wAIx5SdP6M",
+  authDomain: "bookweb-ec140.firebaseapp.com",
+  projectId: "bookweb-ec140",
+  storageBucket: "bookweb-ec140.appspot.com",
+  messagingSenderId: "337804284110",
+  appId: "1:337804284110:web:5a361571ba8c416ae6409f",
+  measurementId: "G-M5YTJ4D33G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
