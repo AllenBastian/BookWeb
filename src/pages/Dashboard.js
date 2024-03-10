88d49a9c067bbfab72a2918db17ba0e9db4aa28d
AllenBastian <reachallenbastian@gmail.com>
2024-03-10 23:54:12 +0530

    added context

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,11 @@
 import { auth } from "../firebase/firebase";
+import { IsSignedUpContext } from "../context/Context";
+import { useContext } from "react";
 
 const Dashboard=()=>{
 
+    const {isSignedUp} = useContext(IsSignedUpContext);
+    console.log(isSignedUp)
     const user = auth.currentUser;
     if (user) {
        console.log(user.email+ "is signed in")
