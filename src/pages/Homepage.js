533d185a9176ddc1e4ba1525f9f968ad35939e45
ARSheretolearn <alinamaryarsam@gmail.com>
2024-04-09 14:45:38 +0530

    Icons added

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Carouselc from '../components/Carousel'; 
-
+import { User, Search, MessageCircle, BookOpen } from 'react-feather';
 
 const Homepage = ()=>{
 
@@ -8,7 +8,7 @@ const Homepage = ()=>{
       <div className="container mx-auto p-4">
       <header className="text-center mb-8">
         <h1 className="text-5xl font-bold">BookWeb</h1>
-        <p className="text-lg text-gray-600">Connect . Share . Read</p>
+        <p className="text-lg text-grey-600">Connect . Share . Read</p>
       </header>
       
       <section className="mb-8">
@@ -24,18 +24,33 @@ const Homepage = ()=>{
       </section>
 
 
-      <section className="pt-20 mb-8">
-        <h2 className="text-2xl text-center text-gray-800 mb-8">How BookWeb Works</h2>
-        <h3 className="text-x1 text-center text-gray-500">1.  Login or Create Your Account</h3>
-        <h3 className="text-x1 text-center text-gray-500">2.  Search for and find your next dream book</h3>
-        <h3 className="text-x1 text-center text-gray-500">3.  Connect with the book owner via WebChat</h3>
-        <h3 className="text-x1 text-center text-gray-500">4.  Get the book and enjoy your reading!</h3>
-      </section>
-      
+   
+
+<section className="pt-20 mb-8">
+    <h2 className="text-2xl text-center text-gray-800 mb-8">How BookWeb Works</h2>
+    <div className="flex justify-center  mb-8 p-10 ">
+        <div className="flex items-center flex-col items-center mr-4">
+            <User className="text-3xl text-red-500 mr-2" />
+            <h3 className="text-x1 text-center text-500 p-5">1.  Login or Create Your Account</h3>
+        </div>
+        <div className="flex items-center flex-col items-center mr-4">
+            <Search className="text-3xl text-red-500 mr-2" />
+            <h3 className="text-x1 text-center text-500 p-5">2.  Search for and find your next dream book</h3>
+        </div>
+        <div className="flex items-center flex-col items-center mr-4">
+            <MessageCircle className="text-3xl text-red-500 mr-2" />
+            <h3 className="text-x1 text-center text-500 p-5">3.  Connect with the book owner via WebChat</h3>
+        </div>
+        <div className="flex items-center flex-col items-center ">
+            <BookOpen className="text-3xl text-red-500 mr-2" />
+            <h3 className="text-x1 text-center text-500 p-5">4.  Get the book and enjoy your reading!</h3>
+        </div>
+    </div>
+</section>
       
       <section>
         <h2 className="text-2xl text-center pt-20">Contact Us</h2>
-        <p className="text-lg">Have any questions or suggestions? Feel free to reach out to us at support.bookweb@gmail.com</p>
+        <p className="text-lg text-center p-10">Have any questions or suggestions? Feel free to reach out to us at support.bookweb@gmail.com</p>
       </section>
     </div>
     )
