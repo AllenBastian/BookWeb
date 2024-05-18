3f33d4b193349f15e3f410e6a021ba082340a828
AllenBastian <reachallenbastian@gmail.com>
2024-05-18 11:00:19 +0530

    image fix

--- src/pages/Learnmore.js
+++ src/pages/Learnmore.js
@@ -1,29 +1,28 @@
 import React from 'react';
 
-
 const teamMembers = [
   {
     name: 'Allen Bastian Joy',
     role: 'Founder & CEO',
-    image: '../../public/images/allen.jpg', // Update with actual image paths
+    image: '/images/allen.jpg', // Update with actual image paths
     description: 'Alice is passionate about books and community building.',
   },
   {  
     name: 'Alina Mary Sam',
     role: 'Chief Technology Officer',
-    image: '../../public/images/ali.jpg', // Update with actual image paths
+    image: '/images/ali.jpg', // Update with actual image paths
     description: 'Bob is the tech wizard making sure everything runs smoothly.',
   },
   {
     name: 'Hridya Syju',
     role: 'Developer',
-    image: '../../public/images/hridya.jpg',
+    image: '/images/hridya.jpg',
     description: 'Hridya is the enthuasist techi.',
   },
   {
     name: 'Beneeta Bency',
     role: 'Manager',
-    image: '../public/images/mwthe.jpg',
+    image: '/images/mwthe.jpg',
     description: 'Beneeta is a developer',
   },
   // Add more team members as needed
@@ -37,9 +36,8 @@ const LearnMore = () => {
       </header>
       <div className="flex flex-wrap justify-center gap-8">
         {teamMembers.map((member, index) => (
-            console.log(member),
           <div key={index} className="w-full md:w-1/3 p-4 text-center bg-white rounded-lg shadow-lg">
-            <img src="./ali.jpg" alt={member.name} className="w-32 h-32 mx-auto rounded-full mb-4" />
+            <img src={member.image} alt={member.name} className="w-32 h-32 mx-auto rounded-full mb-4" />
             <h2 className="text-xl font-bold">{member.name}</h2>
             <p className="text-gray-600 mb-2">{member.role}</p>
             <p className="text-lg">{member.description}</p>
