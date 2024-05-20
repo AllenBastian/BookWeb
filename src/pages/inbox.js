907ab8d89a4a98d5e4b8237f55872032bbc87007
AllenBastian <reachallenbastian@gmail.com>
2024-05-20 23:33:55 +0530

    second tweak

--- src/pages/inbox.js
+++ src/pages/inbox.js
@@ -1,11 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { getFirestore, collection, onSnapshot, query, deleteDoc, doc, orderBy, getDocs, where } from 'firebase/firestore';
-import { getApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { Alert } from '@material-tailwind/react';
-import CustomButton from '../components/CustomButton';
+import React, { useState, useEffect } from "react";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  query,
+  deleteDoc,
+  doc,
+  orderBy,
+  getDocs,
+  where,
+} from "firebase/firestore";
+import { getApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { Alert } from "@material-tailwind/react";
+import { motion, AnimatePresence } from 'framer-motion';
+import CustomButton from "../components/CustomButton"
+import moment from "moment/moment";
+import { FaTimes, FaTrash } from "react-icons/fa";
 
-// Assuming Firebase is already initialized elsewhere in your project
+
 const firestore = getFirestore(getApp());
 
 const Inbox = () => {
@@ -23,97 +36,155 @@ const Inbox = () => {
   }, []);
 
   useEffect(() => {
-    if (!userEmail) return; // Don't proceed if userEmail is not set
-
-    const q = query(collection(firestore, 'notifications'), orderBy('timestamp', 'desc'));
+    if (!userEmail) return; 
+  
+    const q = query(
+      collection(firestore, "notifications"),
+      orderBy("timestamp", "desc")
+    );
+  
     const unsubscribe = onSnapshot(q, (snapshot) => {
       const newNotifications = [];
       snapshot.docChanges().forEach((change) => {
-        if (change.type === 'added') {
+        if (change.type === "added") {
           const notification = change.doc.data();
-          const from = notification.notiffrom;
+          const from = notification.notiffromname;
           const book = notification.title;
           const to = notification.notifto;
-          
-          // Only add notifications meant for the current user
-          if (to === userEmail && change.doc.data().messagetype === "Book Request") {
-            newNotifications.push({ id: change.doc.id, message: `Book request from ${from} for book: ${book}` });
-            
+  
+          if (to === userEmail) {
+            let message = "";
+            switch (notification.messagetype) {
+              case "Book Request":
+                message = `Book request from ${from} for book ${book}`;
+                break;
+              case "Book Request Accepted":
+                message = `Book request for book ${book} has been accepted, you can now chat with the owner`;
+                break;
+              case "Book Request Declined":
+                message = `Book request for book ${book} has been declined.`;
+                break;
+              case "Comment to Post":
+                message = `User ${from} has commented on your post ${book}`;
+                break;
+              case "Review added":
+                message = `User ${from} has reviewed your book ${book}`;
+                break;
+              case "Mark as borrowed":
+                message = `The owner has marked ${book} as borrowed to you.`;
+                break;
+              case "Transaction Complete":
+                message = `The owner has marked ${book} as returned from you.`;
+                break;
+              default:
+                break;
+            }
+  
+            if (message) {
+              newNotifications.push({
+                id: change.doc.id,
+                message,
+                time: moment(new Date(notification.timestamp.seconds * 1000).toLocaleString(), 'DD/MM/YYYY, HH:mm:ss').fromNow()
+              });
+            }
           }
-          else if (notification.messagetype === "Book Request Accepted" && to === userEmail) {
-            newNotifications.push({ id: change.doc.id, message: `Book request for book ${book} has been accepted.` });
         }
-        else if (notification.messagetype === "Book Request Declined" && to === userEmail) {
-          newNotifications.push({ id: change.doc.id, message: `Book request for book ${book} has been declined.` });
-      }
-        else if (notification.messagetype === "Comment to Post" && to === userEmail ){
-          newNotifications.push({id: change.doc.id, message: `User ${from}  has commented on your post ${book}` });
-        }
-        else if (notification.messagetype === "Review added" && to === userEmail ){
-          newNotifications.push({id: change.doc.id, message: `User ${from}  has reviewed your book ${book}` });
-        }
-        }
-
       });
-      setNotifications((prevNotifications) => [...prevNotifications, ...newNotifications]);
+  
+      setNotifications((prevNotifications) => [
+        ...newNotifications,
+        ...prevNotifications
+       
+      ]);
     });
-
-    // Cleanup subscription on unmount
+  
     return () => unsubscribe();
   }, [userEmail]);
+  
+
+  console.log(notifications);
 
   const handleDelete = async (id) => {
     try {
-      await deleteDoc(doc(firestore, 'notifications', id));
-      setNotifications((prevNotifications) => prevNotifications.filter(notification => notification.id !== id));
+      setNotifications((prevNotifications) =>
+        prevNotifications.filter((notification) => notification.id !== id)
+      );
+      await deleteDoc(doc(firestore, "notifications", id));
+     
     } catch (error) {
-      console.error('Error deleting notification:', error);
+      console.error("Error deleting notification:", error);
     }
   };
 
   const handleDeleteAllNotifications = async () => {
     try {
-    
-      // Fetch all notifications for the current user
-      const querySnapshot = await getDocs(query(collection(firestore, 'notifications'), where('notifto', '==', userEmail)));
+      const querySnapshot = await getDocs(
+        query(
+          collection(firestore, "notifications"),
+          where("notifto", "==", userEmail)
+        )
+      );
+
   
-      // Iterate over each notification and delete it
       querySnapshot.forEach(async (doc) => {
         await deleteDoc(doc.ref);
       });
-  
-      // After deleting all notifications, you can clear the notifications state
+
       setNotifications([]);
     } catch (error) {
-      console.error('Error deleting all notifications:', error);
+      console.error("Error deleting all notifications:", error);
     }
   };
-  
-  return (
-    <div className="flex w-full flex-col gap-2 px-4">
-      {notifications.map((notification) => (
-        <Alert key={notification.id} variant="outlined" className="relative">
-          <button
-            onClick={() => handleDelete(notification.id)}
-            className="absolute top-0 right-0 m-2 text-gray-500 hover:text-gray-800"
-            aria-label="Close"
+  if(notifications.length > 0){
+return (
+  <div className="flex w-full flex-col gap-4 px-6 py-4  rounded-lg ">
+    <div className="flex justify-end mt-4">
+        <CustomButton
+          icon={<FaTrash />}
+          className="mr-2"
+          text="Clear Inbox"
+          color="blue"
+          onClick={handleDeleteAllNotifications}
+        />
+      </div>
+      <AnimatePresence>
+        {notifications.map((notification) => (
+          <motion.div
+            key={notification.id}
+            initial={{ opacity: 0, y: -20 }}
+            whileHover={{ scale: 1.01 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.3 }}
           >
-            &times;
-          </button>
-          <span>{notification.message}</span>
-        </Alert>
-        
-      ))}
-      <CustomButton
-                                className="mr-2"
-                                text={"Clear Inbox"}
-                                color={"blue"}
-                                onClick={() => handleDeleteAllNotifications()}
-                                
-                              />
-     
+            <Alert variant="outlined" className="relative bg-gray-200 shadow-sm rounded-lg p-4">
+              <FaTimes
+              size={20}
+                onClick={() => handleDelete(notification.id)}
+                className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
+
+              />
+            
+              <div className="flex flex-col">
+                <span>{notification.message}</span>
+                <span className="text-xs text-blue-700 font-medium mt-2">{notification.time}</span>
+              </div>
+            </Alert>
+          </motion.div>
+        ))}
+      </AnimatePresence>
+      
     </div>
   );
+}
+else
+{
+  return (
+   <div className="text-4xl opacity-30 flex justify-center items-center h-screen ">
+    No new notifications
+   </div>
+  );
+}
 };
 
 export default Inbox;
